43334d053155387d387c49a1a3c557e2b5f7538e
ricky <rickytitus2001@gmail.com>
2022-02-19 15:12:06 -0600

    Made changes to button and list layout

--- app/src/main/java/com/example/outfitoftheday/CustomClothes.kt
+++ app/src/main/java/com/example/outfitoftheday/CustomClothes.kt
@@ -1,11 +1,17 @@
 package com.example.outfitoftheday
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class CustomClothes : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.custom_cloth1)
+        val button2 = findViewById<Button>(R.id.button2)
+        button2.setOnClickListener{
+            finish()
+        }
     }
 }
