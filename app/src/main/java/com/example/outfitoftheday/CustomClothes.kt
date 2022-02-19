a128844258f10285563eb96b27f940167b76e07a
Overheadpillow <60531323+Overheadpillow@users.noreply.github.com>
2022-02-18 19:32:43 -0600

    Adding main screen changes from discord

---
+++ app/src/main/java/com/example/outfitoftheday/CustomClothes.kt
@@ -0,0 +1,11 @@
+package com.example.outfitoftheday
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomClothes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.custom_cloth1)
+    }
+}
--- app/src/main/java/com/example/outfitoftheday/MainActivity.kt
+++ app/src/main/java/com/example/outfitoftheday/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.outfitoftheday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button1 = findViewById<Button>(R.id.button1)
+        button1.setOnClickListener{
+            val intent = Intent(this, CustomClothes::class.java)
+            startActivity(intent)
+        }
     }
 }
