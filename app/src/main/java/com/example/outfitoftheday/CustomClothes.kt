56f8b365bad526d37b1bad0409bb52c5d6fe824e
ricky <rickytitus2001@gmail.com>
2022-02-21 12:03:43 -0600

    Connected Result Screen to Recycler View

--- app/src/main/java/com/example/outfitoftheday/CustomClothes.kt
+++ app/src/main/java/com/example/outfitoftheday/CustomClothes.kt
@@ -21,17 +21,19 @@ class CustomClothes : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.adapter = ItemAdapter(context = this, myDataset, isBlue)
         recyclerView.setHasFixedSize(true)
-        val resultButton = findViewById<Button>(R.id.result_button)
-        resultButton.setOnClickListener{
-            val intent = Intent(this, ResultScreen::class.java)
-            startActivity(intent)
-        }
         val button2 = findViewById<Button>(R.id.button2)
         val button3 = findViewById<Button>(R.id.button3)
         button2.setOnClickListener {
             finish()
         }
         button3.setOnClickListener {
+            val intent = Intent(this, ResultScreen::class.java)
+            intent.putExtra("hat", isBlue[0].toString())
+            intent.putExtra("shirt", isBlue[1].toString())
+            intent.putExtra("jacket", isBlue[2].toString())
+            intent.putExtra("pants", isBlue[3].toString())
+            intent.putExtra("shoes", isBlue[4].toString())
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/outfitoftheday/ResultScreen.kt
+++ app/src/main/java/com/example/outfitoftheday/ResultScreen.kt
@@ -1,24 +1,48 @@
 package com.example.outfitoftheday
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
-import com.example.outfitoftheday.adapter.ItemAdapter
-import com.example.outfitoftheday.data.Datasource
 
 class ResultScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.end_screen)
-        val myDataset = Datasource().loadQuotes()
-        val recyclerView = findViewById<RecyclerView>(R.id.result_recycle)
-        recyclerView.adapter = ItemAdapter(context = this, myDataset, CustomClothes().isBlue)
-        recyclerView.setHasFixedSize(true)
+        val hat = intent.getStringExtra("hat").toBoolean()
+        val shirt = intent.getStringExtra("shirt").toBoolean()
+        val jacket = intent.getStringExtra("jacket").toBoolean()
+        val pants = intent.getStringExtra("pants").toBoolean()
+        val shoes = intent.getStringExtra("shoes").toBoolean()
+        if (!hat){
+            val hatColor = findViewById<ImageView>(R.id.hatView)
+            hatColor.setImageResource(R.drawable.red)
+        }
+        if (!shirt){
+            val shirtColor = findViewById<ImageView>(R.id.shirtView)
+            shirtColor.setImageResource(R.drawable.red)
+        }
+        if (!jacket){
+            val jacketColor = findViewById<ImageView>(R.id.jacketView)
+            jacketColor.setImageResource(R.drawable.red)
+        }
+        if (!pants){
+            val pantsColor = findViewById<ImageView>(R.id.pantsView)
+            pantsColor.setImageResource(R.drawable.red)
+        }
+        if (!shoes){
+            val shoeColor = findViewById<ImageView>(R.id.shoesView)
+            shoeColor.setImageResource(R.drawable.red)
+        }
+
 
         val restart = findViewById<Button>(R.id.restartButton)
         restart.setOnClickListener {
-            finish()
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
         }
 
         val button2 = findViewById<Button>(R.id.button2)
